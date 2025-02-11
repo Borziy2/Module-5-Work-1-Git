5a2bc385d1127c550cc29adbe1ecbdd56e0f4dee
Danil Doroshenko <doroshenkodanil272@gmail.com>
2025-02-11 20:46:09 +0700

    Task 10 lesson

--- Module5-server/src/database/index.js
+++ Module5-server/src/database/index.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 
 module.exports = async () => {
-  await mongoose.connect(
-    "mongodb+srv://admin:admin@nodeblogwork.yi7rf.mongodb.net/?retryWrites=true&w=majority&appName=NodeBlogWork"
-  );
+  await mongoose.connect(process.env.MONGO_CONNECTION);
 
   console.log("MongoDb connected!");
 };
